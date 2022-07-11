e5cc38325dd23712a31354c50dc0a78ad6e0193e
Augusto Nunes <gutonunes99@gmail.com>
2022-07-11 20:34:07 -0300

    fast fast fast

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id :expire_after
+			session[:user_id] = @user.id
 
 			redirect_to '/deposit'
 		else
