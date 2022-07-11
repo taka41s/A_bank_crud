64b0dcadacb4b66e751493f3418ce433d20941e0
Augusto Nunes <gutonunes99@gmail.com>
2022-07-11 16:00:54 -0300

    added more frotnend functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id
+			session[:user_id] = @user.id :expire_after
 
 			redirect_to '/deposit'
 		else
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-			params.require(:user).permit(:email, :password)
+			params.require(:user).permit(:email, :password, :name)
 	end
 
 	def user_edit_params
