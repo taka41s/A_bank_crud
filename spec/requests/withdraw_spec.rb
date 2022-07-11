e59014f08731b7fc95e6c7f40ed32d1c391d46b2
Augusto Nunes <gutonunes99@gmail.com>
2022-07-10 23:01:45 -0300

    created some unit test and refactoring withdraw controller

--- spec/requests/withdraw_spec.rb
+++ spec/requests/withdraw_spec.rb
@@ -3,22 +3,19 @@ require 'rails_helper'
 RSpec.describe "Transactions", type: :request do
   before do
     @user = User.create!({id: 1, email: 'test', password: 'test', current_balance: 2000.0})
-    @target_user = User.create!({id: 2, email: 'test2', password: 'test2', current_balance: 2000.0})
     post(login_path, params: {user: {email: 'test', password: 'test'}})
   end
 
   describe "POST /transactions" do
     it "transactions working" do
       user_params = {
-        to_user: @target_user.email,
         amount: 500.0,
       }
-      post(transactions_path, params: {user: user_params})
+      post(withdraw_path, params: {user: user_params})
       @user.reload
-      @target_user.reload
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(204)
+      expect(@user.wallet_balance).to eq(500.0)
       expect(@user.current_balance).to eq(1500.0)
-      expect(@target_user.current_balance).to eq(2500.0)
     end
   end
 end
